d996462436bc5b973d259dbe6a0da40adb204e90
Ikollec  Yang <ikollecyang@gmail.com>
2026-05-16 09:44:57 +0800

    book index
    a book index producer which using java

---
+++ Book/src/Main.java
@@ -0,0 +1,11 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+Book THRREBODY=new Book("三体",14263889,"刘慈欣",100);
+System.out.println("这本书的名字叫做："+THRREBODY.name);
+System.out.println("这本书的作者是："+THRREBODY.author);
+System.out.println("这本书的编码是："+THRREBODY.NBS);
+        }
+
+}
